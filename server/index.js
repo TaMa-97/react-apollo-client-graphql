64758054763f61b79243cd803a568d2f05bc46e8
Lilly <myk19977@gmail.com>
2023-05-18 23:54:28 +0900

    :fire: バックエンド側 Mutation定義 / メッセージの追加

--- server/index.js
+++ server/index.js
@@ -1,16 +1,32 @@
 const { ApolloServer, gql } = require("apollo-server");
 
-// スキーマ定義
+let messages = []; // メッセージを保存する配列
+
+// typeDefs（スキーマ定義：クライアントがサーバーに問い合わせ可能なクエリやデータの変更操作を定義するもの）
 const typeDefs = gql`
   type Query {
     hello: String
+    messages: [String]
+  }
+  type Mutation {
+    addMessage(message: String): String
   }
 `;
 
-// リゾルバ定義
+// resolvers（リゾルバ定義：リゾルバはクエリに対する答え方を定義するもの）
 const resolvers = {
   Query: {
     hello: () => "Hello, world!",
+    messages: () => messages, // 全てのメッセージを返すリゾルバ
+  },
+  // Mutation（GraphQL）：サーバー上のデータを変更する操作
+  Mutation: {
+    addMessage: (_, { message }) => {
+      //第一引数は現在のオブジェクトの状態、第二引数はクライアントから送られてくるデータ
+      // メッセージを追加するリゾルバ
+      messages.push(message);
+      return message;
+    },
   },
 };
 
